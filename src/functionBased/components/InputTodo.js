045f530d1a5fefc4c084839ffdad89fd27793855
Natasha Heng <natashahengjy@gmail.com>
2023-02-10 16:29:03 +0800

    section 8

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,30 +1,32 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import { FaPlusCircle } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 const InputTodo = (props) => {
   const [inputText, setInputText] = useState({
     title: "",
-  })
+  });
 
-  const onChange = e => {
-    setInputText(prevState => {
+  const onChange = (e) => {
+    setInputText((prevState) => {
       return {
         ...prevState,
         [e.target.name]: e.target.value,
-      }
-    })
-  }
+      };
+    });
+  };
 
-  const handleSubmit = e => {
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (inputText.title.trim()) {
-      props.addTodoProps(inputText.title)
+      props.addTodoProps(inputText.title);
       setInputText({
         title: "",
-      })
+      });
     } else {
-      alert("Please write item")
+      alert("Please write item");
     }
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit} className="form-container">
@@ -36,8 +38,20 @@ const InputTodo = (props) => {
         name="title"
         onChange={onChange}
       />
-      <button className="input-submit">Submit</button>
+      <IconContext.Provider
+        value={{
+          color: "darkcyan",
+          style: { fontSize: "20px", color: "#ff0000" },
+          className: "submit-icon",
+        }}
+      >
+        <button className="input-submit">
+          <FaPlusCircle />
+          <FaPlusCircle />
+          <FaPlusCircle />
+        </button>
+      </IconContext.Provider>
     </form>
-  )
-}
-export default InputTodo
+  );
+};
+export default InputTodo;
